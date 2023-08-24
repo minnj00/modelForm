284bb3a64c618cd8c25331a83c86b8c3fd30eb05
KangMinju <demo1208@gmail.com>
2023-08-24 13:00:48 +0900

    up

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 
@@ -19,3 +20,29 @@ def detail(request, id):
 
     }
     return render(request, 'detail.html', context)
+
+def create(request):
+    # 사용자가 입력한 데이터를 가져와서 DB에 저장
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        # ArticleForm은 request.POST에서 가져온 데이터들을 
+        # 알맞은 위치에 title 과 content를 넣어줌.
+        # 또 required 와 같이 적합하지 않은 데이터가 들어오는 것을 막아주는 html코드 자동 작성
+        # 여기까지는 frontend의 역할
+
+        # 백엔드에서 한번 더 적합한 데이터인지 검증
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', id=article.id)
+        
+
+    # 사용자가 데이터를 입력할 수 있도록 빈 종이를 리턴 
+    else: 
+        form = ArticleForm()
+    
+        context = {
+            'form': form,
+
+        }
+
+    return render(request, 'create.html', context)
