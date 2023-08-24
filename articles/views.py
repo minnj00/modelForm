c6bba756b3bda674b1e179cd96da39494ae0c6bd
KangMinju <demo1208@gmail.com>
2023-08-24 13:45:19 +0900

    create 기능구현(2차)

--- articles/views.py
+++ articles/views.py
@@ -22,20 +22,32 @@ def detail(request, id):
     return render(request, 'detail.html', context)
 
 def create(request):
+    # create 에 해당하는 기능 
     # 사용자가 입력한 데이터를 가져와서 DB에 저장
     if request.method == 'POST':
         form = ArticleForm(request.POST)
-        # ArticleForm은 request.POST에서 가져온 데이터들을 
+        # ArticleForm은 request.POST에서 가져온 데이터들을(title, content)
         # 알맞은 위치에 title 과 content를 넣어줌.
         # 또 required 와 같이 적합하지 않은 데이터가 들어오는 것을 막아주는 html코드 자동 작성
         # 여기까지는 frontend의 역할
 
-        # 백엔드에서 한번 더 적합한 데이터인지 검증
-        if form.is_valid():
+        # 백엔드에서 한번 더 적합한 데이터인지 검증(프론트엔드는 얼마든지 조작 가능하므로!!)
+        # .is_valid() -> T, F 로 출력
+        if form.is_valid(): 
             article = form.save()
             return redirect('articles:detail', id=article.id)
         
+        # 만약 잘못된 정보를 입력하였다면 잘 입력한 칸은 유지해줘야 좋음.
+        else: 
+            # form = ArticleForm()
 
+            context = {
+                'form': form,
+            
+            }
+            return render(request, 'create.html', context)
+        
+    # new 에 해당하는 기능
     # 사용자가 데이터를 입력할 수 있도록 빈 종이를 리턴 
     else: 
         form = ArticleForm()
@@ -45,4 +57,5 @@ def create(request):
 
         }
 
-    return render(request, 'create.html', context)
+        return render(request, 'create.html', context)
+    
