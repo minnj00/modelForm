ae5ed8f9ed4a286d578c26f8c876f99397fa773f
KangMinju <demo1208@gmail.com>
2023-08-24 10:50:47 +0900

    update

---
+++ articles/urls.py
@@ -0,0 +1,14 @@
+# import 잊지말기
+from django.urls import path
+from . import views 
+ 
+# 가끔 서버 킨 상태에서 새로 만들면 반영이 안될 수도 있으니 
+# module 에러가 나면 다시 서버 껏다 켜보기도 하기
+
+
+app_name = 'articles'
+
+urlpatterns=[
+    path('',views.index, name='index'),
+    path('<int:id>/',views.detail, name='detail'),
+]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+# 인덱스가 하는 일은 게시물들을 보여주는 것
+def index(request):
+    articles = Article.objects.all()
+
+    context ={
+        'articles': articles
+    }
+    return render(request, 'index.html', context )
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+    context ={
+        'article': article,
+
+    }
+    return render(request, 'detail.html', context)
